f9034b635dc89da0eec6563b1a55b2a9655c0bb5
aioz <dong.van.le@aioz.io>
2025-04-04 15:46:37 +0700

    Change server to AIOZ

--- internal/protocols/httpp/handler_server_header.go
+++ internal/protocols/httpp/handler_server_header.go
@@ -10,6 +10,6 @@ type handlerServerHeader struct {
 }
 
 func (h *handlerServerHeader) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Server", "mediamtx")
+	w.Header().Set("Server", "AIOZ")
 	h.Handler.ServeHTTP(w, r)
 }
